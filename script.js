23eb300571b78fcd2a8f897c37752dd0fa41fa97
MOHAMEDHANIFAHARISH <mohamedhanifaharish1@gmail.com>
2024-03-13 00:13:10 +0530

    frist commit

---
+++ script.js
@@ -0,0 +1,25 @@
+/*alert("welcome");
+console.log(12344);
+console.log(52.25);
+console.log({ame:'jon',age:89});
+console.table({ame:'jon',age:89});
+console.warn("warning");
+console.error("error");
+console.time("t");
+for(i=0;i<294;i++)
+{console.log(i)};
+console.timeEnd("t");
+
+var a=25;
+console.log(a)
+let b=45
+console.log(b)
+const c=55
+console.log(c)
+const st={name:'hi',age:25}
+console.log(st)
+st.name="abi"
+console.log(st)
+*/
+var d=new Date()
+console.log(d)
